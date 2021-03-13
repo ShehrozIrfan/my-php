e3633ab92da9ce6635392d1f2b32ce335fed7dd9
ShehrozIrfan <email@example.com>
2021-03-13 21:37:30 +0500

    creating a method

--- 13.classes.php
+++ 13.classes.php
@@ -2,12 +2,24 @@
 
 class Car {
     
+    //creating a method.
+    function moveWheels() {
+        echo "Wheels Move...";
+    }
 }
 
+//checking whether a class exists or not.
 if(class_exists("Car")) {
     echo "Class Exists";
 } else {
     echo "Doesn't exist";
+}
+
+//checking whether a function inside a class exists or not.
+if(method_exists("Car", "moveWheels")) {
+    echo "<br>" . "Move Wheels method exists..";
+} else {
+    echo "<br>" . "Move Wheels method doesn't exists...";
 }
 
 ?>
