546152b5f54729c583472cafe1036dba8119ebaf
ShehrozIrfan <email@example.com>
2021-03-16 10:39:18 +0500

    Adding properties to class

--- 13.classes.php
+++ 13.classes.php
@@ -2,9 +2,16 @@
 
 class Car {
     
+    //adding properties to our class.
+    var $wheels = 4;
+    var $doors = 5;
+    var $engine = 1;
+    
     //creating a method.
     function moveWheels() {
         echo "Wheels Move...";
+        //if we want to access a property inside a method, then we use 'this'
+        $this -> wheels = 8;
     }
 }
 
@@ -30,5 +37,12 @@ echo "<br>";
 $honda -> moveWheels();
 echo "<br>";
 $toyota -> moveWheels();
+
+echo "<br>";
+//so to access the properties:
+echo $honda -> wheels;
+//we can also re-assign the values of properties.
+$honda -> wheels = 5;
+echo "<br>" . $honda -> wheels;
 
 ?>
